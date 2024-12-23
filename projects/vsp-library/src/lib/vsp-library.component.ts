7680880c6262ef2a2fa9dc50ff4b4ca307e00257
viniciussoares1 <vinicius.sds04@gmail.com>
2024-12-23 02:51:45 -0300

    feat: adicionando app e lib

---
+++ projects/vsp-library/src/lib/vsp-library.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-vsp-library',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      vsp-library works!
+    </p>
+  `,
+  styles: ``
+})
+export class VspLibraryComponent {
+
+}
---
+++ projects/vsp-library/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of vsp-library
+ */
+
+export * from './lib/vsp-library.service';
+export * from './lib/vsp-library.component';
